33dfe6ff396f829bb892fb977d59673250a5eaba
nekane <mixhardcore@hotmail.com>
2025-11-19 11:53:20 +0100

    pantalla de añadir con codigo

--- MiniReto/Form2.vb
+++ MiniReto/Form2.vb
@@ -1,9 +1,34 @@
 ﻿Public Class Form2
-    Private Sub lbSpeedrun_Click(sender As Object, e As EventArgs) Handles lbSpeedrun.Click
 
-    End Sub
+    Dim gestor As New Fichero("videojuegos.txt")
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles tbTitulo.TextChanged
+    Private Sub btAñadir_Click(sender As Object, e As EventArgs) Handles btAñadir.Click
+
+        Dim titulo As String
+        Dim plataformas As String
+        Dim estudio As String
+        Dim speedrun As Double
+        Dim speedrunner As String
+        Dim año As Integer
+
+
+        titulo = tbTitulo.Text
+        plataformas = tbPlataforma.Text
+        estudio = tbPlataforma.Text
+        speedrun = tbSpeedrun.Text
+        speedrunner = tbSpeedrunner.Text
+        año = tbAño.Text
+
+        gestor.CrearFicheroNuevo()
+
+        Dim videojuego As New Videojuego(titulo, plataformas, estudio, año, speedrun, speedrunner)
+
+        gestor.aniadirRegistro(videojuego)
+
+
+
+
+
 
     End Sub
 End Class
