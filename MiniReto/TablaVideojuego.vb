e1a94aa28fa0aecce666dc24a62f04d387fb1e00
xabidefuentes <xabidefu@gmail.com>
2025-11-19 11:43:26 +0100

    Tabla videojuegos

--- MiniReto/TablaVideojuego.vb
+++
@@ -1,6 +0,0 @@
-﻿Imports System.IO
-Imports System.ComponentModel
-
-Public Class TablaVideojuego
-
-End Class
---
+++ MiniReto/TablaVideojuegos.Designer.vb
@@ -0,0 +1,36 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TablaVideojuegos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' TablaVideojuegos
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Name = "TablaVideojuegos"
+        Text = "TablaVideojuegos"
+        ResumeLayout(False)
+    End Sub
+End Class
