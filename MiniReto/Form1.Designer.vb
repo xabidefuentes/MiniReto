0e8a718233bc06004bd05535bc99bcb549c7e871
xabidefuentes <xabidefu@gmail.com>
2025-11-20 10:32:02 +0100

    Tabla videojuegos arreglada

---
+++ MiniReto/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        lbVideojuegos = New ListBox()
+        btnGuardar = New Button()
+        btnEliminar = New Button()
+        SuspendLayout()
+        ' 
+        ' lbVideojuegos
+        ' 
+        lbVideojuegos.FormattingEnabled = True
+        lbVideojuegos.Location = New Point(134, 39)
+        lbVideojuegos.Name = "lbVideojuegos"
+        lbVideojuegos.Size = New Size(476, 204)
+        lbVideojuegos.TabIndex = 0
+        ' 
+        ' btnGuardar
+        ' 
+        btnGuardar.Location = New Point(134, 316)
+        btnGuardar.Name = "btnGuardar"
+        btnGuardar.Size = New Size(169, 44)
+        btnGuardar.TabIndex = 1
+        btnGuardar.Text = "Button1"
+        btnGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEliminar
+        ' 
+        btnEliminar.Location = New Point(441, 303)
+        btnEliminar.Name = "btnEliminar"
+        btnEliminar.Size = New Size(169, 44)
+        btnEliminar.TabIndex = 2
+        btnEliminar.Text = "ELIMINAR"
+        btnEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnEliminar)
+        Controls.Add(btnGuardar)
+        Controls.Add(lbVideojuegos)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents lbVideojuegos As ListBox
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnEliminar As Button
+End Class
---
+++ MiniReto/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+Public Class Form1
+
+    ' Array de videojuegos
+    Private listaVideojuegos(99) As Videojuego
+    Private cantidad As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Cargar ejemplos al iniciar
+        AñadirEjemplos()
+        ActualizarListBox()
+    End Sub
+
+    Private Sub AñadirEjemplos()
+        listaVideojuegos(cantidad) = New Videojuego("Super Mario 64", "Nintendo 64", "Nintendo", 1996, 3899, "cheese")
+        cantidad += 1
+
+        listaVideojuegos(cantidad) = New Videojuego("Portal", "PC", "Valve", 2007, 366, "CantEven")
+        cantidad += 1
+
+        listaVideojuegos(cantidad) = New Videojuego("Minecraft", "PC", "Mojang", 2011, 1320, "Illumina")
+        cantidad += 1
+    End Sub
+
+    Private Sub ActualizarListBox()
+        lbVideojuegos.Items.Clear()
+
+        For i As Integer = 0 To cantidad - 1
+            lbVideojuegos.Items.Add(
+                $"{listaVideojuegos(i).titulo} | " &
+                $"{listaVideojuegos(i).plataforma} | " &
+                $"{listaVideojuegos(i).estudio} | " &
+                $"{listaVideojuegos(i).anio} | " &
+                $"{listaVideojuegos(i).tiempoSpeedrun} | " &
+                $"{listaVideojuegos(i).recordista}"
+            )
+        Next
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+        If lbVideojuegos.SelectedIndex = -1 Then
+            MessageBox.Show("Selecciona un videojuego para eliminar.")
+            Return
+        End If
+
+        Dim index As Integer = lbVideojuegos.SelectedIndex
+
+        ' Desplazar el array hacia arriba
+        For i As Integer = index To cantidad - 2
+            listaVideojuegos(i) = listaVideojuegos(i + 1)
+        Next
+
+        cantidad -= 1
+
+        ActualizarListBox()
+    End Sub
+
+    Private Sub btnGuardar_Click_1(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Form2.Show()
+    End Sub
+End Class
